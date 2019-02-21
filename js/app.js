ac9fb2ecb6033efd5c5ceb413296a2066270e9b5
Alexkcshing <alexkcshing@gmail.com>
2019-02-21 15:49:11 +0000

    end of lab

---
+++ js/app.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript loaded');
+
+  const form = document.querySelector("#new-item-form");
+
+  form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+
+
+    const title = event.target.title.value;
+    const author = event.target.author.value;
+    const category = event.target.category.value;
+
+    // console.dir()
+
+    const newBook = document.createElement("div");
+    const newBookTitle = document.createElement("div");
+    const newBookAuthor = document.createElement("div");
+    const newBookCategory = document.createElement("div");
+
+    newBookTitle.textContent = title;
+    newBookAuthor.textContent = author;
+    newBookCategory.textContent = category;
+
+    newBook.appendChild(newBookTitle);
+    newBookTitle.classList.add("book-title");
+    newBook.appendChild(newBookAuthor);
+    newBookAuthor.classList.add("book-author");
+    newBook.appendChild(newBookCategory);
+    newBookCategory.classList.add("new-category");
+
+
+    newBook.classList.add("new-item");
+
+    const readingList = document.querySelector("#reading-list");
+    readingList.appendChild(newBook);
+
+
+    form.reset();
+  });
+
+  const deleteAll
+
+
+
+
+
+
+
+})
